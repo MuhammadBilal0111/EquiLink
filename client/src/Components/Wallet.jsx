ece6430fd2b3a02da33927765b3eb355c2b65faa
Muhammad Bilal <m.bilal0111@gmail.com>
2025-03-17 19:58:34 +0500

    storing the wallet address in the store

--- client/src/Components/Wallet.jsx
+++ client/src/Components/Wallet.jsx
@@ -3,13 +3,19 @@ import Button from "./elements/Button";
 import { connectWallet } from "@/services/metaMask";
 import { useState } from "react";
 import { FaEthereum } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { profileActions } from "@/store";
 
 const Wallet = () => {
-  const [account, setAccount] = useState(null);
+  const dispatch = useDispatch();
+  const { walletAddress } = useSelector((state) => state?.profileStore);
+  console.log("wallet", walletAddress);
+
   const handleWalletConnection = async () => {
     const walletAddress = await connectWallet();
-    setAccount(walletAddress);
+    dispatch(profileActions.setWalletAddress(walletAddress));
   };
+
   return (
     <div className="flex min-h-screen w-full items-center justify-center text-white">
       <div className="w-full max-w-md rounded-2xl p-8 shadow-lg">
@@ -19,9 +25,10 @@ const Wallet = () => {
           <p className="text-gray-400 text-center">
             Securely connect your Metamask wallet to access blockchain features.
           </p>
-          {account ? (
+          {walletAddress ? (
             <div className="w-full rounded-lg bg-gray-800 p-2 text-center text-lg font-medium text-green-400">
-              Connected: {account.substring(0, 6)}...{account.slice(-4)}
+              Connected: {walletAddress?.substring(0, 9)}...
+              {walletAddress?.slice(-4)}
             </div>
           ) : (
             <Button
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,39 +1,40 @@
-import {configureStore, createSlice} from "@reduxjs/toolkit"
-
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: "User",
-    initialState: { authUser: null },
-    reducers: {
-      setUser: (state, action) => {
-        state.authUser = action.payload;
-      },
+  name: "User",
+  initialState: { authUser: null },
+  reducers: {
+    setUser: (state, action) => {
+      state.authUser = action.payload;
     },
+  },
 });
 
 const profileSlice = createSlice({
-  name:"Profile",
-  initialState:{profile:null},
-  reducers:{
-    setProfile:(state,action)=>{
-      state.profile = action.payload
-    }
+  name: "Profile",
+  initialState: { profile: null, walletAddress: null },
+  reducers: {
+    setProfile: (state, action) => {
+      state.profile = action.payload;
+    },
+    setWalletAddress: (state, action) => {
+      state.walletAddress = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(userSlice.actions.setUser, (state, action) => {
       state.profile = action.payload; // Update profile when authUser is set
     });
   },
-})
+});
 
-
-
-const store = configureStore({reducer:{
+const store = configureStore({
+  reducer: {
     userStore: userSlice.reducer,
-    profileStore: profileSlice.reducer
-}});
-
+    profileStore: profileSlice.reducer,
+  },
+});
 
 export const userActions = userSlice.actions;
-export const profileActions = profileSlice.actions
+export const profileActions = profileSlice.actions;
 export default store;
